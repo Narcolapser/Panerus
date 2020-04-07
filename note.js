e4fc53ca63d203d5a4e5d5b09afa88ae020569bf
Toben Archer <sandslash@gmail.com>
2020-04-06 20:06:16 -0500

    Got notes.
    I've got a basic system for notes in place. It doesn't handle 8th
    notes or diacritical marking yet, and it really throws the UI for a
    loop every time you open and close the selection menu, but it is a
    start. I want to save now while it is working before I go onto the
    next part of this.

--- note.js
+++ note.js
@@ -1,24 +1,55 @@
 import React from 'react';
+import onClickOutside from "react-onclickoutside";
 import { StyleSheet, Text, View } from 'react-native';
 
-export function Note() {
-  function changeNote(e) {
-    e.preventDefault();
-    console.log('Note was clicked')
+export class Note extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      left_diacritic: '',
+      left_note: '',
+      right_note: '',
+      right_diacritic: '',
+      notes: '·',
+      menuOpen: false
+    }
   }
+  handleClickOutside(){
+    this.setState({
+      menuOpen: false
+    })
+  }
+  toggleMenu(){
+    this.setState(prevState => ({
+      menuOpen: !prevState.menuOpen
+    }))
+  }
+  select_note(val){
+    this.setState({
+      menuOpen: false,
+      left_note: val,
+      notes: val
+    })
+  }
+  render(){
   return (
-    <span>
-      <select onClick={changeNote} style="">
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-        <option>4</option>
-        <option>5</option>
-        <option>6</option>
-        <option>7</option>
-      </select>
+    <span className="dd-wrapper">
+      <span className="note-header" onClick={() => this.toggleMenu()}>
+        <span className="dd-header-title"> {this.state.notes} </span>
+      </span>
+      {this.state.menuOpen && <ul className="dd-list">
+        <li className="dd-list-item" onClick={() => this.select_note(' ')}> </li>
+        <li className="dd-list-item" onClick={() => this.select_note('·')}>·</li>
+        <li className="dd-list-item" onClick={() => this.select_note('1')}>1</li>
+        <li className="dd-list-item" onClick={() => this.select_note('2')}>2</li>
+        <li className="dd-list-item" onClick={() => this.select_note('3')}>3</li>
+        <li className="dd-list-item" onClick={() => this.select_note('4')}>4</li>
+        <li className="dd-list-item" onClick={() => this.select_note('5')}>5</li>
+        <li className="dd-list-item" onClick={() => this.select_note('6')}>6</li>
+        <li className="dd-list-item" onClick={() => this.select_note('7')}>7</li>
+      </ul>}
     </span>
-  );
+  )}
 }
 
 const styles = StyleSheet.create({
