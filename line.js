9b8a1e46270dba91a46e46a9416ef40ecae1461b
Toben Archer <sandslash@gmail.com>
2020-05-04 21:14:16 -0500

    I hate it when I forget new files.
    Added lines. I actually added this like 3 commits ago. Anyway. I decided
    to use lines as part of the structure of the code since that was the
    level that the editor would want to work with. This allows for the
    potential of interlacing lines from multiple instruments. I'm not sure
    if I'll permit that behavior in the editor, we shall see.

---
+++ line.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Gatra } from './gatra.js';
+
+export class Line extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  update_gatra(gatra, value)
+  {
+    console.log('Updating the ' + gatra + ' to ' + value);
+    let gatras = this.props.gatras.split('\t');
+    gatras[gatra] = value;
+    console.log('Updating line to: ' + gatras.join('\t'));
+    this.props.onChange(gatras.join('\t'));
+  }
+  render(){
+  let gatras = this.props.gatras.split('\t')
+  return (
+    <View className="dd-wrapper" style={{backgroundColor: '#eee', minWidth: 25, float: 'left'}}>
+      <Gatra notes={gatras[0]} onChange={(value) => this.update_gatra(0,value)}></Gatra>
+      <Gatra notes={gatras[1]} onChange={(value) => this.update_gatra(1,value)}></Gatra>
+      <Gatra notes={gatras[2]} onChange={(value) => this.update_gatra(2,value)}></Gatra>
+      <Gatra notes={gatras[3]} onChange={(value) => this.update_gatra(3,value)}></Gatra>
+    </View>
+  )}
+}
