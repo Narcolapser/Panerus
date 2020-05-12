8deb86a1157deff66b93e3052bb6db0cdd413a9b
Toben Archer <sandslash@gmail.com>
2020-05-11 20:20:46 -0500

    Trimming my plans.
    I had believed I was going to need a slide in menu, but I'm not sure of
    that at this point. I'm going to try to get navigation and a list of
    documents to work next.

--- screens/homescreen.js
+++ screens/homescreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BackHandler } from 'react-native';
 import NavBar, { NavGroup, NavButton, NavButtonText, NavTitle } from 'react-native-nav';
 
 export default class HomeScreen extends React.Component {
@@ -6,15 +7,22 @@ export default class HomeScreen extends React.Component {
     return (
       <NavBar>
         <NavGroup>
-          <NavButton onPress={() => alert('hi')}>
+          <NavButton onPress={() => alert('Coming soon.')}>
             <NavButtonText>
-              {"Menu"}
+              {"New..."}
             </NavButtonText>
           </NavButton>
         </NavGroup>
           <NavTitle>
             {"Panerus"}
           </NavTitle>
+          <NavGroup>
+            <NavButton onPress={() => BackHandler.exitApp()}>
+              <NavButtonText>
+                {"Exit"}
+              </NavButtonText>
+            </NavButton>
+          </NavGroup>
       </NavBar>
     )
   }
