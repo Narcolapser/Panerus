79a2e817dc5e8c168adb082510179a1cc874c82f
Toben Archer <sandslash@gmail.com>
2020-05-18 20:41:26 -0500

    Got some basic documents in a scroll view.
    To make sure that things were working before I moved on I made some
    dummy document with a for loop and verified that the scroll view works.
    Fantastic. Now lets open a new document eh?

--- App.js
+++ App.js
@@ -2,20 +2,37 @@ import * as React from 'react';
 import { StyleSheet, Text, View, Button, PermissionsAndroid, ScrollView } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 //import {SongScreen} from 'screens/song.js';
 
+function Document(props){
+  return (
+    <View style={styles.document} key={props.name}>
+      <Text>{props.name}</Text>
+    </View>
+    )
+}
+
 function HomeScreen({ navigation }) {
+  let docs = [];
+  //<Icon name="plus-circle-outline"/>
+  let plus_icon = <Icon name="plus-circle-outline"/>;
+  docs.push(Document({"name":plus_icon}));
+
+  for(let i = 0; i < 10; i++)
+    docs.push(Document({"name":i}))
+
   return (
     <View style={styles.root}>
-      <View style={styles.sideBar}><Text>Left</Text></View>
+      <View style={styles.side_bar}><Text>Left</Text></View>
       <View style={styles.middle}>
-        <Text>Home Screen1</Text>
-        <Button
-          title="Go to Song"
-          onPress={() => navigation.navigate('Song')}
-        />
+        <ScrollView style={styles.document_scroll}>
+          <Text>Top2</Text>
+          { docs }
+          <Text>Bottom</Text>
+        </ScrollView>
       </View>
-      <View style={styles.sideBar}><Text>Right</Text></View>
+      <View style={styles.side_bar}><Text>Right</Text></View>
     </View>
   );
 }
@@ -44,32 +61,13 @@ function App() {
 export default App;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    //marginTop: 20,
-    backgroundColor: 'grey',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  scrollView: {
-    backgroundColor: 'pink',
-    marginHorizontal: 20,
-    width: '50%'
-  },
-  text: {
-    fontSize: 42,
-  },
-  navBar: {
-    flex: 1,
-    width: '100%',
-  },
   root: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row'
   },
-  sideBar:{
+  side_bar:{
     flex: 1,
     backgroundColor: 'grey',
     height: '100%'
@@ -78,5 +76,17 @@ const styles = StyleSheet.create({
     flex: 2,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  document:{
+    width: "100%",
+    height: 200,
+    borderWidth: 4,
+    borderColor: "#000",
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  document_scroll: {
+    backgroundColor: 'pink',
+    width: "100%",
   }
 });
